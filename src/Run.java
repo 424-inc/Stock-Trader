441e5acf359b8cdbe3d366a823d981041fa43d07
logan42474 <loganmiller040402@gmail.com>
2019-07-31 16:35:34 -0700

    Added Telegram JAR files
    Maven couldn't collect the dependency because there was an issue with it

--- src/Run.java
+++ src/Run.java
@@ -1,4 +1,3 @@
-
 public class Run {
 	// Base directory so that all of the Information is stored in an organized manner
 	// Desktop for now for simplicity
@@ -6,7 +5,6 @@ public class Run {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		Read_And_Write.Startup();
-		Read_And_Write.File_Manipulation.WriteToFile("Hello", "test");
 	}
 
 }
