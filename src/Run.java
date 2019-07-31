682a44004eecfa2644b9e7f9f8c30a07333b8bb4
logan42474 <loganmiller040402@gmail.com>
2019-07-31 15:28:18 -0700

    Switched Everything to Maven

--- src/Run.java
+++ src/Run.java
@@ -4,7 +4,9 @@ public class Run {
 	// Desktop for now for simplicity
 	public static String basedir = System.getProperty("user.home")+"/Desktop/StockTrader";
 	public static void main(String[] args) {
+		System.out.println("Test");
 		Read_And_Write.Startup();
+		Read_And_Write.File_Manipulation.WriteToFile("Hello", "test");
 	}
 
 }
