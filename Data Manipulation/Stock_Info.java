af2e6c0f0aa096cb592ee73f025b941787b91a6c
logan42474 <loganmiller040402@gmail.com>
2019-08-14 12:34:15 -0700

    Simple Yahoo Services quotes

--- Data Manipulation/Stock_Info.java
+++ Data Manipulation/Stock_Info.java
@@ -1,20 +1,27 @@
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+import yahoofinance.quotes.stock.StockQuote;
 
 public class Stock_Info {
-public static void Current_Price() throws IOException {
-	Stock stock = YahooFinance.get("INTC");
+public static BigDecimal Current_Price(String Stock) throws IOException {
+	Stock stock = YahooFinance.get(Stock);
 	 
-	BigDecimal price = stock.getQuote().getPrice();
-	BigDecimal change = stock.getQuote().getChangeInPercent();
-	BigDecimal peg = stock.getStats().getPeg();
-	BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
-	 
-	stock.print();
+	return stock.getQuote().getPrice();
+}
+public static ArrayList<HistoricalQuote> Historical_Price(String Stock, int Years) throws IOException {
+	Calendar from = Calendar.getInstance();
+	Calendar to = Calendar.getInstance();
+	from.add(Calendar.YEAR, -Years);
+
+	Stock google = YahooFinance.get(Stock);
+	return (ArrayList<HistoricalQuote>) google.getHistory(from, to, Interval.DAILY);
 }
 }
--- src/Run.java
+++ src/Run.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
+
+import yahoofinance.histquotes.HistoricalQuote;
 
 public class Run {
 	// Base directory so that all of the Information is stored in an organized manner
@@ -6,7 +9,15 @@ public class Run {
 	public static String basedir = System.getProperty("user.home")+"/Desktop/StockTrader";
 	public static void main(String[] args) throws IOException {
 		Read_And_Write.Startup();
-		Stock_Info.Current_Price();
+		// Simple example to use Yahoo Services
+		while(true) {
+		System.out.println(Stock_Info.Current_Price("BTC-USD"));
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		}
 	}
 
 }
