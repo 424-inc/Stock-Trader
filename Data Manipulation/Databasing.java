c4fa2f974a32da611faa316f9adb724ba493a28b
logan42474 <loganmiller040402@gmail.com>
2019-08-15 18:41:09 -0700

    WAY faster way of getting data on an Array of stocks

--- Data Manipulation/Databasing.java
+++ Data Manipulation/Databasing.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
+import yahoofinance.Stock;
 
 public class Databasing {
 	private static boolean recording;
@@ -13,20 +11,12 @@ public class Databasing {
 			recording=true;
 			Thread thread = new Thread() {
 				public void run() {
-					HashMap<String,ArrayList<BigDecimal>> data = new HashMap<String, ArrayList<BigDecimal>>();
-					for(int i=0;i<Database.Stock_Names.size();i++) {
-						data.put(Database.Stock_Names.get(i), new ArrayList<BigDecimal>());
-					}
 					while(recording) {
+						Map<String, Stock> stocks = Stock_Info.Current_Price_Array(Database.Stock_Names);
 						for(int i=0;i<Database.Stock_Names.size();i++) {
-							try {
-								BigDecimal value = Stock_Info.Current_Price(Database.Stock_Names.get(i));
-								data.get(Database.Stock_Names.get(i)).add(value);
-								System.out.println(Database.Stock_Names.get(i)+": "+value);
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
+							System.out.println(Database.Stock_Names.get(i)+": "+stocks.get(Database.Stock_Names.get(i)).getQuote().getPrice());
 						}
+						System.out.println("----------------------------\n----------------------------\n----------------------------\n");
 						try {
 							Thread.sleep(1000*60);
 						} catch (InterruptedException e) {
--- Data Manipulation/Stock_Info.java
+++ Data Manipulation/Stock_Info.java
@@ -2,7 +2,9 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
@@ -15,6 +17,16 @@ public static BigDecimal Current_Price(String Stock) throws IOException {
 	Stock stock = YahooFinance.get(Stock);
 	 
 	return stock.getQuote().getPrice();
+}
+public static Map<String, Stock> Current_Price_Array(ArrayList<String> symbols){
+	String[] sym = Database.Stock_Names.toArray(new String[0]);
+	Map<String, Stock> stocks = null;
+	try {
+		stocks = YahooFinance.get(sym);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	return stocks;
 }
 public static ArrayList<HistoricalQuote> Historical_Price(String Stock, int Years) throws IOException {
 	Calendar from = Calendar.getInstance();
